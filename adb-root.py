7822e05586520e5d0ad5b2413bf62a89341b154f
Jake <git@ja-ke.tech>
2018-09-20 13:11:14 +0200

    Variable loglevels

--- adb-root.py
+++ adb-root.py
@@ -1,9 +1,6 @@
 #!/bin/env python
 import argparse, subprocess, logging
 
-logging.basicConfig(format='[%(relativeCreated)d ms] %(levelname)s:%(message)s')
-logging.getLogger().setLevel("INFO") #TODO parameter
-
 parser = argparse.ArgumentParser(description="read/write files as root on any Android device")
 parser.add_argument("action", choices=["push", "pull"], help="pull to copy from device, push to copy to device")
 parser.add_argument("source", help="path of the file to copy")
@@ -11,7 +8,16 @@ parser.add_argument("target", help="destination for the copied file")
 parser.add_argument("-m", "--mode", help="set mode of file (chmod notation)")
 parser.add_argument("-o", "--owner", help="set owner and group of file (chown notation)")
 parser.add_argument("-c", "--check", help="calculate and compare hashsum after transfer",  action="store_true")
+parser.add_argument('--verbose', '-v', action='count', help="Increase verbosity from default warning level")
+parser.add_argument('--verbosity', choices=["DEBUG", "INFO", "WARNING", "ERROR"], help="Directly set logging level")
 args = parser.parse_args()
+
+
+loglevel = args.verbosity if args.verbosity else 30
+if args.verbose:
+    loglevel -= args.verbose*10
+    
+logging.basicConfig(format='[%(relativeCreated)d ms] %(levelname)s:%(message)s', level=loglevel)
 
 
 def log_exitcode(action, result):    
