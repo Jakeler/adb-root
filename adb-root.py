5327a41c974cf0738fb6c2a0234286c938f5ddf2
Jake <git@ja-ke.tech>
2018-09-20 02:56:06 +0200

    Move hash check into method

--- adb-root.py
+++ adb-root.py
@@ -2,7 +2,7 @@
 import argparse, subprocess, logging
 
 logging.basicConfig(format='[%(relativeCreated)d ms] %(levelname)s:%(message)s')
-logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel("INFO") #TODO parameter
 
 parser = argparse.ArgumentParser(description="read/write files as root on any Android device")
 parser.add_argument("action", choices=["push", "pull"], help="pull to copy from device, push to copy to device")
@@ -21,6 +21,15 @@ def log_exitcode(action, result):
     else:
         logging.error("Error, exitcode: %d", code)
     logging.debug(result)
+    
+def hash_check(local, remote):
+        logging.info("Started integrity check with SHA256")
+        hash_src = subprocess.run(["sha256sum", local], stdout=subprocess.PIPE).stdout.decode('utf-8')[:64]
+        hash_dest = subprocess.run(['adb', 'shell', "su -c", "sha256sum", remote], stdout=subprocess.PIPE).stdout.decode('utf-8')[:64]
+        if hash_src == hash_dest:
+            logging.info("Integrity ok: %s => %s (%s)" % (local, remote, hash_src))
+        else:
+            logging.error("Check failed, hashsum mismatch: %s vs %s" % (hash_src, hash_dest))
 
 def push():
     logging.info("Started pushing %s", args.source)
@@ -36,12 +45,7 @@ def push():
         log_exitcode("Change owner", result)
     
     if args.check:
-        hash_src = subprocess.run(["sha256sum", args.source], stdout=subprocess.PIPE).stdout.decode('utf-8')[:64]
-        hash_dest = subprocess.run(['adb', 'shell', "su -c", "sha256sum", args.target], stdout=subprocess.PIPE).stdout.decode('utf-8')[:64]
-        if hash_src == hash_dest:
-            print("Integrity ok: %s => %s (%s)" % (args.source, args.target, hash_src))
-        else:
-            print("Check failed, hashsum mismatch: %s vs %s" % (hash_src, hash_dest))
+        hash_check(args.source, args.target)
 
 
 def pull():
@@ -50,13 +54,8 @@ def pull():
         result = subprocess.run(['adb', 'shell', "su -c", "dd if="+args.source], stdout=file)
         log_exitcode("Transfer", result)
         
-    if args.check: #TODO merge in method
-        hash_src = subprocess.run(['adb', 'shell', "su -c", "sha256sum", args.source], stdout=subprocess.PIPE).stdout.decode('utf-8')[:64]
-        hash_dest = subprocess.run(["sha256sum", args.target], stdout=subprocess.PIPE).stdout.decode('utf-8')[:64]
-        if hash_src == hash_dest:
-            print("Integrity ok: %s => %s (%s)" % (args.source, args.target, hash_src))
-        else:
-            print("Check failed, hashsum mismatch: %s vs %s" % (hash_src, hash_dest))
+    if args.check:
+        hash_check(args.target, args.source)
             
     if args.mode or args.owner:
         logging.warning("mode/owner ignored for pull!")
